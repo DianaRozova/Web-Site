558dba82bf54bd695e4a9c9c605ea35de6de11dd
DianaRozova <rozova.diana19@gmail.com>
2023-04-10 15:13:15 +0300

    add link from server

--- src/stores/mainlinks.js
+++ src/stores/mainlinks.js
@@ -1,7 +1,15 @@
-import { defineStore } from 'pinia'
+import { defineStore } from 'pinia';
 import data from './listOfLinks.json' assert { type: 'JSON' };
+import { getListLinks } from '../serverHelpers/links';
 
-export const listOfLinks = defineStore('links', () => {
-  let list = data.array;
-  return { list }
-})
+export const listOfLinks = defineStore('links', async () => {
+  let list = data;
+  await getListLinks().then((serverData) => {
+    console.log(serverData);
+    list = serverData;
+  }).catch((err) => {
+    console.log(err);
+  });
+  return { list };
+});
+
